66a324985a94da393bfbdcfbdfa36b782ec18bdd
ttnha <ttnha@ttnha.tma.com.vn>
2015-04-17 14:09:23 +0700

    delete st

--- src/demo/test.java
+++ src/demo/test.java
@@ -1,24 +1,6 @@
 package demo;
 
 public class test {
-	// thay doi trong code
-	//ttnha thay doi bla bla
-	//win 74 thay doi 
-	//dt
-	
-	//win94
-	//win 94 co branch: ttnha
-	
-	//win 94 co branch: ttnha hehe
-	
-	//win 94 co branch: ttnha hehe
-	
-	
-	//local co branch: ttnha
-	
-	//win 94 co branch: win94
-	
-	//local co branch: ttnha
-	
+		
 	
 }
